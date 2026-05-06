6ac288c8177755517fb34a6b20df0afcf5a87ef6
o.prymak <o.prymak@andersenlab.com>
2026-05-06 15:39:46 +0200

    fix: update base URL in vite.config.ts to prevent 404 errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/worst-ux/', // This fixes the 404 by prefixing all paths
     plugins: [react()],
-    // IMPORTANT: This must match your repo name exactly
-    base: '/worst-ux/',
 })
