a878097dd595102190932c26beecf3a4cbc7967d
o.prymak <o.prymak@andersenlab.com>
2026-05-06 15:28:46 +0200

    add deployment scripts and configure base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://peckeby.github.io/worst-ux/",
+  base:  "/worst-ux/",
   plugins: [react()],
 });
