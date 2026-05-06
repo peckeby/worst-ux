9045e0c12b36a11bfc83ec7c3ddaee93094003c6
o.prymak <o.prymak@andersenlab.com>
2026-05-06 15:37:33 +0200

    add deployment scripts and configure base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/worst-ux/', // This ensures paths become /worst-ux/src/main.tsx
+    // IMPORTANT: This must match your repo name exactly
+    base: '/worst-ux/',
 })
